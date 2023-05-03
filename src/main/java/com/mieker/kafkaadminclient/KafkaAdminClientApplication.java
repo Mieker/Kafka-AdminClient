4a73edfd843c68b7f4bf17f97ae1ff4044ce2c3e
Mieker <mieker@wp.pl>
2023-05-03 17:28:38 +0200

    kafka adminClient created

---
+++ src/main/java/com/mieker/kafkaadminclient/KafkaAdminClientApplication.java
@@ -0,0 +1,50 @@
+package com.mieker.kafkaadminclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.ListTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+@SpringBootApplication
+@Slf4j
+public class KafkaAdminClientApplication {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        SpringApplication.run(KafkaAdminClientApplication.class, args);
+
+        Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        AdminClient adminClient = AdminClient.create(properties);
+
+        Set<String> topics = adminClient.listTopics().names().get();
+
+        for (String topic : topics) {
+            log.info(topic);
+        }
+
+        NewTopic thirdTopic = new NewTopic("third_topic", 3, (short) 1);
+
+        adminClient.createTopics(List.of(thirdTopic)).all().get();
+
+        for (String topic : topics) {
+            log.info(topic);
+        }
+
+//        adminClient.deleteTopics(Collections.singleton("third_topic"));
+//
+//        for (String topic : topics) {
+//            log.info(topic);
+//        }
+    }
+
+}
---
+++ src/test/java/com/mieker/kafkaadminclient/KafkaAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mieker.kafkaadminclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAdminClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
