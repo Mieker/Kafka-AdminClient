471b733dbcb70495db0444d0f66d0aed6be7d4e0
Mieker <mieker@wp.pl>
2023-05-03 21:18:40 +0200

    css style added for welcome page; implementation of topic creation added in controller

--- src/main/java/com/mieker/kafkaadminclient/KafkaAdminClientApplication.java
+++ src/main/java/com/mieker/kafkaadminclient/KafkaAdminClientApplication.java
@@ -3,16 +3,11 @@ package com.mieker.kafkaadminclient;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.ListTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
@@ -21,20 +16,6 @@ public class KafkaAdminClientApplication {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         SpringApplication.run(KafkaAdminClientApplication.class, args);
-
-        Set<String> topics = adminClient.listTopics().names().get();
-
-        for (String topic : topics) {
-            log.info(topic);
-        }
-
-//        NewTopic thirdTopic = new NewTopic("third_topic", 3, (short) 1);
-//
-//        adminClient.createTopics(List.of(thirdTopic)).all().get();
-//
-//        for (String topic : topics) {
-//            log.info(topic);
-//        }
 
 //        adminClient.deleteTopics(Collections.singleton("third_topic"));
 //
@@ -45,8 +26,6 @@ public class KafkaAdminClientApplication {
 
     @Bean
     public AdminClient getAdminClient() {
-
-        //TODO: setup Bean for controller to extract variables to create new topics
         Properties properties = new Properties();
         properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         return AdminClient.create(properties);
--- src/main/java/com/mieker/kafkaadminclient/controller/AdminClientController.java
+++ src/main/java/com/mieker/kafkaadminclient/controller/AdminClientController.java
@@ -1,20 +1,49 @@
 package com.mieker.kafkaadminclient.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class AdminClientController {
 
-    @PostMapping("/submit")
-    public String submitForm(@RequestParam String stringValue, @RequestParam int intValue, @RequestParam short shortValue) {
-        // tutaj możesz wykonać dowolną logikę biznesową z otrzymanymi wartościami
+    private final AdminClient adminClient;
 
-        log.info(stringValue + intValue + shortValue);
-        return "success"; // zwrócenie widoku potwierdzającego, że formularz został wysłany
+    @PostMapping("/create-topic")
+    public String createTopic(
+            @RequestParam String topicName, @RequestParam int partitionsNumber, @RequestParam short replicasNumber)
+            throws ExecutionException, InterruptedException {
+
+        log.info(topicName + partitionsNumber + replicasNumber);
+
+//        NewTopic thirdTopic = new NewTopic(topicName, partitionsNumber, replicasNumber);
+//
+//        adminClient.createTopics(List.of(thirdTopic)).all().get();
+
+        return "redirect:/create-topic";
+    }
+
+    @PostMapping("/delete-topic")
+    public String deleteTopic(@RequestParam String topicName) {
+
+        //TODO: implementation of deleting topics here
+
+        return null;
+    }
+
+    private void logAllTopics() throws ExecutionException, InterruptedException {
+        Set<String> topics = adminClient.listTopics().names().get();
+        topics.forEach(log::info);
     }
 
 }
